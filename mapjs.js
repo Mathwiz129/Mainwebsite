e326e1cdfaa263e474ccf5ac67d1d1bb6ab6c996
Mathwiz129 <brandontoakes@gmail.com>
2024-01-17 11:11:55 -0500

    less ideal

--- mapjs.js
+++ mapjs.js
@@ -14,10 +14,10 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(dataResponse => {
                 data = dataResponse;
                 markers = L.layerGroup().addTo(map);
-
+    
                 data.forEach(function (team) {
                     var teamsAtLocation = getTeamsAtLocation(team.lat, team.lon);
-
+    
                     var marker = L.marker([team.lat, team.lon]).addTo(markers);
 
                     if (teamsAtLocation.length > 1) {
@@ -52,9 +52,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     marker.on('click', function () {
                         var latlng = marker.getLatLng();
                         var teamsAtLocation = getTeamsAtLocation(latlng.lat, latlng.lng);
-
+    
                         resetSidebar();
-
+    
                         if (teamsAtLocation.length > 0) {
                             updateSidebar(teamsAtLocation);
                         } else {
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         zoomToTeam(team);
                     });
                 });
-
+    
                 resetSidebar();
             })
             .catch(error => {
